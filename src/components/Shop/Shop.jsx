01d5080464079095f8a419f1d16c91e9de52266f
ualiurrahat <ualiurrahat25@gmail.com>
2023-04-09 22:47:47 +0600

    display each product image,name in product layout

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './Shop.css'
+import Product from "../Product/Product";
 
 const Shop = () => {
     // loading data
@@ -12,7 +13,9 @@ const Shop = () => {
   return (
     <div className="shop-container">
       <div className="products-container">
-        <h2>Products coming here: {products.length}</h2>
+        {
+            products.map(product => <Product key = {product.id} product = {product}></Product>)
+        }
       </div>
       <div className="cart-container">
         <h4>Order Summary</h4>
