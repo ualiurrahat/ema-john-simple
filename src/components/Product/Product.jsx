52d4405bec4f18543c3f8935452fa05bc88be083
ualiurrahat <ualiurrahat25@gmail.com>
2023-04-13 01:26:32 +0600

    styled product cart and button cart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import './Product.css'
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-    const {img,name,seller,price,quantity} = props.product;
-    return (
-        <div className='product'>
-           <img src={img} alt="" />
-           <h6>{name}</h6>
-        </div>
-    );
+  const { img, name, seller, price, ratings } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <h6 className="product-name">{name}</h6>
+      <div className="product-info">
+        <p>Price:${price}</p>
+        <p>Manufacturer:{seller}</p>
+        <p>Rating: {ratings} Stars</p>
+      </div>
+      <button className="btn-cart">Add to Cart</button>
+    </div>
+  );
 };
 
 export default Product;
