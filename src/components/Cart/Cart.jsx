adda365f6c0ec9491d0bdcd4d795f1ceafbfb8e4
ualiurrahat <ualiurrahat25@gmail.com>
2023-04-15 15:00:46 +0600

    calculatet total price, tax, total purchase and showed in order summary

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import './Cart.css'
+import "./Cart.css";
 const Cart = (props) => {
-    const {cart} = props;
-    let totalPrice = 0;
-    for(const product of cart)
-    {
-        totalPrice += product.price;
-    }
+  const { cart } = props;
+  let totalPrice = 0;
+  let totalShipping = 0;
+  for (const product of cart) {
+    totalPrice += product.price;
+    totalShipping += product.shipping;
+  }
+  // calculate 7 % tax
+  const tax = (totalPrice * 7) / 100;
+  const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div className="cart">
       <h4>Order Summary</h4>
       <p>Total Products:{cart.length}</p>
-      <p>Total Price:{totalPrice}</p>
-      <p>Total Shipping:</p>
-      <p>Tax:</p>
-      <h5>Grand Total:</h5>
+      <p>Total Price: ${totalPrice}</p>
+      <p>Total Shipping: ${totalShipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
     </div>
   );
 };
